2556fe2f819947cbb735d24180169912468ffa7d
Andreea <andrea.iosip@gmail.com>
2020-10-15 13:54:22 +0000

    Tea and Coffee modules registered in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import Coffee, Tea
 
-# Register your models here.
-admin.site.register(Coffee)
-admin.site.register(Tea)
 
+# Register your models here.
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'strength',
+        'image',
+        'price',
+    )
+
+    ordering = ('strength',)
+
+
+class TeaAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'ingredients',
+        'teabags',
+        'image',
+        'price',
+    )
+    ordering = ('price',)
+
+
+admin.site.register(Coffee, CoffeeAdmin)
+admin.site.register(Tea, TeaAdmin)
