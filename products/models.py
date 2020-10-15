285659e7ce5b907213a91c160a84aba0df4023c0
Andreea <andrea.iosip@gmail.com>
2020-10-15 08:50:18 +0000

    Field name changed in the Tea model.

--- products/models.py
+++ products/models.py
@@ -18,10 +18,11 @@ class Tea(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     tea_bags = models.CharField(max_length=20)
-    leaf_type = models.CharField(max_length=254, default='')
+    ingredients = models.CharField(max_length=254, default='')
     image = models.ImageField(upload_to='static/img')
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 def __str__(self):
     return self.name
+
