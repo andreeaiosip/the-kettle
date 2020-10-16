fd30a57b6555894acb796b9b1cd53ca6d85a9066
Andreea <andrea.iosip@gmail.com>
2020-10-16 00:06:42 +0000

    Coffee products view created

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Tea, Coffee
 
 # Create your views here.
+
+
+def all_coffee(request):
+
+    coffee = Coffee.objects.all()
+
+    context = {
+        'coffee': coffee,
+    }
+
+    return render(request, 'products/coffee.html', context)
+
