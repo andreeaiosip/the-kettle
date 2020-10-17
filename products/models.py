0c4196983967bc85aa9ec8fa668d91a1ac12d42e
Andreea <andrea.iosip@gmail.com>
2020-10-17 18:50:49 +0000

    No of tea bags field changed to integer.

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Coffee(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
@@ -10,21 +11,19 @@ class Coffee(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Tea(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    tea_bags = models.CharField(max_length=20)
+    tea_bags = models.IntegerField()
     ingredients = models.CharField(max_length=254, default='')
     image = models.ImageField(upload_to='static/img')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
