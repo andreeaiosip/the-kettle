7a7a67f7cd2f1be5e629d5e79212a0204d75b0d7
Andreea <andrea.iosip@gmail.com>
2020-10-17 22:53:55 +0000

    Category field added to Product Model.

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
+    category = models.CharField(max_length=10, default='Coffee')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='static/img')
