c4e977f26d37c412d891e19adb62db46e637c5b9
Andreea <andrea.iosip@gmail.com>
2020-10-17 23:02:58 +0000

    Tea and Coffee category options added.

--- products/models.py
+++ products/models.py
@@ -4,8 +4,18 @@ from django.db import models
 
 
 class Product(models.Model):
+
+    TEA = 'Tea'
+    COFFEE = 'Coffee'
+    CATEGORY_CHOICES = [
+        (TEA, 'Tea'),
+        (COFFEE, 'Coffee'),
+    ]
+    category = models.CharField(
+        max_length=10,
+        choices=CATEGORY_CHOICES,
+    )
     name = models.CharField(max_length=254, default='')
-    category = models.CharField(max_length=10, default='Coffee')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='static/img')
