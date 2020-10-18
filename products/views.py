5afceb19f0b5589b7e981f2881f0f43d29b8b124
Andreea <andrea.iosip@gmail.com>
2020-10-18 11:15:14 +0000

    Single product view set up.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
+
+# All products view
 
 
 def all_products(request):
@@ -13,4 +15,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-    
+
+
+# One product view
+
+def product_view(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_view.html', context)
