1a4354a7e0abebc2524bb676b44b578e27b68830
Andreea <andrea.iosip@gmail.com>
2020-10-18 12:59:43 +0000

    Search query set in search bar.

--- products/views.py
+++ products/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.db.models import Q
+from django.contrib import messages
 from .models import Product
-
 # Create your views here.
 
-# All products view
+# All products view and search query
 
 
 def all_products(request):
 
     products = Product.objects.all()
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
