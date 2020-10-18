2525de481667cb0c58df490273a2f4a4d25ac413
Andreea <andrea.iosip@gmail.com>
2020-10-18 15:09:59 +0000

    Search query and filtering is working

--- products/views.py
+++ products/views.py
@@ -1,29 +1,38 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
-from django.db.models import Q
+  
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from .models import Product
-# Create your views here.
+from django.db.models import Q
+from .models import Product, Category
 
-# All products view and search query
+# Create your views here.
 
 
 def all_products(request):
+    """ All products and search query in search bar. """
 
     products = Product.objects.all()
     query = None
+    categories = None
+
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-                queries = Q(name__icontains=query) | Q(description__icontains=query)
-                products = products.filter(queries)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
@@ -32,7 +41,7 @@ def all_products(request):
 # One product view
 
 def product_view(request, product_id):
-    """ A view to show individual product details """
+    """ Show details of only one product."""
 
     product = get_object_or_404(Product, pk=product_id)
 
