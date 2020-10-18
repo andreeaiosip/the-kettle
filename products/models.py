57a8a94e290b8ac346f22f1afe40f63dafbc5dda
Andreea <andrea.iosip@gmail.com>
2020-10-18 15:08:42 +0000

    Category Model returns friendly name.

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# Product model
 
 class Product(models.Model):
 
@@ -23,8 +24,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
