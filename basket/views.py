fa607075e2515a9959159d5d9465fb612436a90c
Andreea <andrea.iosip@gmail.com>
2020-10-19 18:04:56 +0000

    Add products to Basket view and url created.

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def view_basket(request):
     """ Shopping bag is returned """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('basket', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['basket'] = bag
+    print(request.session['basket'])
+    return redirect(redirect_url)
