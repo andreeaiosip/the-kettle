20f44e948c7cb826b724140b9b4f1ebc258a13b3
Andreea <andrea.iosip@gmail.com>
2020-10-20 14:12:56 +0000

    Friendly name removed from Category and Flavour model.

--- products/models.py
+++ products/models.py
@@ -27,13 +27,10 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=20)
-    friendly_name = models.CharField(max_length=20, null=True, blank=False)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 # Flavour model
@@ -41,11 +38,9 @@ class Category(models.Model):
 class Flavour(models.Model):
 
     name = models.CharField(max_length=20)
-    friendly_name = models.CharField(max_length=20, null=True, blank=False)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+
 
