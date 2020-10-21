6231fefafd6ae6d61d4ceb836093a562f82e8ab3
Andreea <andrea.iosip@gmail.com>
2020-10-21 23:44:21 +0000

    App name updated

--- the_kettle/settings.py
+++ the_kettle/settings.py
@@ -111,11 +111,16 @@ WSGI_APPLICATION = 'the_kettle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DATABSE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
 }
 
 
