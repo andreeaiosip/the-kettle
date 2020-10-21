f667fc012c869fe1d0bf1f82f1cb7acb50b8e90b
Andreea <andrea.iosip@gmail.com>
2020-10-21 14:51:07 +0000

    Update basket view and url is created.

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<item_id>/', views.update_basket, name='update_basket'),
 ]
