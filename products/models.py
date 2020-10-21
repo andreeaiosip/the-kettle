75422f2237bb4870216167485a99762dc73d65ae
Andreea <andrea.iosip@gmail.com>
2020-10-21 14:46:46 +0000

    Category friendly name is restored.

--- products/models.py
+++ products/models.py
@@ -19,18 +19,19 @@ class Product(models.Model):
         return self.name
 
 
-# Category model
-
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 # Flavour model
