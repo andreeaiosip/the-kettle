8333110d7e24b05117d1c2c39c9a48935e1a3cd0
Andreea <andrea.iosip@gmail.com>
2020-10-26 23:44:44 +0000

    Models code rearranged.

--- products/models.py
+++ products/models.py
@@ -2,22 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-# Product model
-
-
-class Product(models.Model):
-
-    name = models.CharField(max_length=254, default='')
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    flavour = models.ForeignKey('Flavour', null=True, blank=True, on_delete=models.SET_NULL)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='static/img')
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
+# Category Model
 
 class Category(models.Model):
 
@@ -39,6 +24,26 @@ class Category(models.Model):
 class Flavour(models.Model):
 
     name = models.CharField(max_length=20)
+    friendly_name = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# Product model
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=254, default='')
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    flavour = models.ForeignKey('Flavour', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='static/img')
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
