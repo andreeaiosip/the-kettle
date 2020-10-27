d1477cc1f8546aee9942f25309bc6cdf29033746
Andreea <andrea.iosip@gmail.com>
2020-10-27 12:57:28 +0000

    Small typo corrected.

--- the_kettle/settings.py
+++ the_kettle/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-kettle.herokuapp.com', 'locahost']
+ALLOWED_HOSTS = ['the-kettle.herokuapp.com', 'localhost']
 
 
 # Application definition
