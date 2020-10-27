cb23d5e5832cb20d16688e5c9f29fa249ce7b067
Andreea <andrea.iosip@gmail.com>
2020-10-27 12:28:16 +0000

    Initial commit in Geroky.

--- the_kettle/settings.py
+++ the_kettle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i^x58*+-t576qqt7s-r5mi7@@adi88zqy_!2=)u19ly*wfm2eb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
