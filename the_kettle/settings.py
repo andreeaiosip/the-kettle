7400392b8a0ae1fb4d9358c7ffb5e262809477be
Andreea <andrea.iosip@gmail.com>
2020-10-27 13:56:28 +0000

    Custome storages removed.

--- the_kettle/settings.py
+++ the_kettle/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-kettle.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['the-kettle.herokuapp.com', 'locahost']
 
 
 # Application definition
