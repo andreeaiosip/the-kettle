131cd7aedc8bc1ecc95d07463fdfc541ae103f8d
Andreea <andrea.iosip@gmail.com>
2020-10-31 00:34:44 +0000

    Checkout page is implemented.

---
+++ checkout/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'street_address1',
+            'street_address2',
+            'town_or_city',
+            'country',
+            )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Placeholders and classes are added, auto-generated
+        labels are removed, set autofocus on first field input.
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
+            
