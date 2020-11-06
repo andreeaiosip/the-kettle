b4e34425172b7917702915aa3e503cd25710ab89
Andreea <andrea.iosip@gmail.com>
2020-11-06 14:52:00 +0000

    guardian 1

--- checkout/models.py
+++ checkout/models.py
@@ -15,9 +15,10 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    # original_basket = models.TextField(null=False, blank=False, default='')
+    # stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
