5f837f3d78741b18b1fb785f93d6b7f837d9b4d9
Andreea <andrea.iosip@gmail.com>
2020-11-07 23:47:19 +0000

    Admin is able to edit/update products.

--- products/views.py
+++ products/views.py
@@ -69,3 +69,24 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit products' details """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('product_view', args=[product.id]))
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
