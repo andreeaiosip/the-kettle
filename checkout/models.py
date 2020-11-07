366bd2775da6b0f25d894eaebf53d3d701fcd675
Andreea <andrea.iosip@gmail.com>
2020-11-07 16:20:24 +0000

    User profile created and connected to orders.

--- checkout/models.py
+++ checkout/models.py
@@ -5,10 +5,13 @@ from django.conf import settings
 from products.models import Product
 
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -20,7 +23,6 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_basket = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
-
 
     def _generate_order_number(self):
         """
