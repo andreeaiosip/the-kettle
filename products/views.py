fe5a95690b5276e52e16eb1f2a71e7f0e30d7eb1
Andreea <andrea.iosip@gmail.com>
2020-11-08 00:03:48 +0000

    Small links for edit/delete added to products view.

--- products/views.py
+++ products/views.py
@@ -58,8 +58,8 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect(reverse('add_product'))
+            product = form.save()
+            return redirect(reverse('product_view', args=[product.id]))
     else:
         form = ProductForm()
 
@@ -90,3 +90,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the database """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    return redirect(reverse('products'))
